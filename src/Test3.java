6f7827f74eb823701f2760bdbbffdcd34ece85a9
Fathir Fathan <fathirfathan706.ff@gmail.com>
2022-01-01 10:34:17 +0700

    commit on new branch

---
+++ src/Test3.java
@@ -0,0 +1,7 @@
+public class Test3 {
+    public static void main(String[] args) {
+        System.out.println("Hello 3");
+        System.out.println("Hello 3");
+        System.out.println("Hello 3");
+    }
+}
