18c9e8f7e3dd93193978c2a7bfd2ad4868404819
Fathir Fathan <fathirfathan706.ff@gmail.com>
2022-01-01 10:40:55 +0700

    removed file and changed Main.java

--- src/Test3.java
+++
@@ -1,7 +0,0 @@
-public class Test3 {
-    public static void main(String[] args) {
-        System.out.println("Hello 3");
-        System.out.println("Hello 3");
-        System.out.println("Hello 3");
-    }
-}
